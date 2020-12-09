c3502e090dbc2a7fdf97ceac498542bee40b4ee9
haku-kiro <botprime@yahoo.com>
2020-12-09 20:29:20 +0200

    delete func, todo - finish

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'weather'
+
 urlpatterns = [
-    path('', views.index, name="weather_index")
+    path('', views.index, name="weather_index"),
+    path('<int:pk>/', views.delete, name="delete")
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
+from django.http.response import HttpResponse
 from the_weather.local_settings import API_KEY
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
 
@@ -13,24 +14,33 @@ def index(request):
         form.save()
 
     form = CityForm()
-
     cities = City.objects.all()
-
     weather_data = []
 
     for city in cities:
         # Make sure to add your api key from openweathermap.org
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city.name}&units=metric&appid={API_KEY}"
-        r = requests.get(url).json()
 
-        city_weather = {
-            'city': city.name,
-            'temperature': r['main']['temp'],
-            'description': r['weather'][0]['description'],
-            'icon': r['weather'][0]['icon'],
-        }
+        try:
+            r = requests.get(url).json()
 
-        weather_data.append(city_weather)
+            city_weather = {
+                'city': city.name,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'],
+            }
+            weather_data.append(city_weather)
+        except Exception as e:
+            print(e)
+            print(f'failed to collect data for city: {city.name}')
 
     context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
+
+
+def delete(request, pk):
+    city = City.objects.get(pk=pk)
+    # city.delete()
+
+    return HttpResponse(request, "Test")
